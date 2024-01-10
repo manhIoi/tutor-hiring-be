c70b882a80460d13ba0eab38ed25a3239f2c9fe7
manhIoi <manhloi0505@gmail.com>
2024-01-10 23:47:28 +0700

    chore: convert socket

--- src/main-service/socket/index.ts
+++ src/main-service/socket/index.ts
@@ -3,7 +3,7 @@ import ChatDataSource from "../datasource/chatDataSource";
 import RoomChatDataSource from "../datasource/roomChatDataSource";
 import NotificationDataSource from "../datasource/notificationDataSource";
 
-const chatSocket = new ChatSocket(3000, {
+const chatSocket = new ChatSocket(4000, {
   chatDataSource: new ChatDataSource(),
   roomChatDataSource: new RoomChatDataSource(),
   notificationDataSource: new NotificationDataSource(),
